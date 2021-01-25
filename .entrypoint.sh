ab53d48c942172cd1469d2d4c5dd1008fa0fa514
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2021-01-25 19:06:39 +0900

    Update Dockerfile and .entrypoint.sh script

--- .entrypoint.sh
+++ .entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 source "/opt/lge-ros2/install/setup.bash"
-exec ros2 launch cloisim_ros_bringup "$@"
-# exec ros2 run cloisim_ros_bringup cloisim_ros_bringup "$@"
+echo ROS_DOMAIN_ID=$ROS_DOMAIN_ID
+exec ros2 run cloisim_ros_bringup cloisim_ros_bringup "$@"
