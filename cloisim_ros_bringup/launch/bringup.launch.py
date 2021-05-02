95370683147e93f2f541deb7ef7261614b32b588
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2021-05-02 17:28:49 +0900

    Add new launch argument for cloisim_ros_bringup - can change scan topic name

--- cloisim_ros_bringup/launch/bringup.launch.py
+++ cloisim_ros_bringup/launch/bringup.launch.py
@@ -19,11 +19,13 @@ def generate_launch_description():
     _single_mode = LaunchConfiguration('single_mode')
     _target_model = LaunchConfiguration('target_model')
     _target_parts = LaunchConfiguration('target_parts')
+    _scan = LaunchConfiguration('scan')
 
     cloisim_ros_cmd = Node(
         package="cloisim_ros_bringup",
         executable="cloisim_ros_bringup",
         output='screen',
+        remappings=[('scan',_scan)],
         parameters=[{'single_mode': _single_mode, 'target_model': _target_model, 'target_parts': _target_parts}])
 
     declare_launch_argument_sm = DeclareLaunchArgument(
@@ -41,6 +43,11 @@ def generate_launch_description():
         default_value='',
         description='specify the target parts you want')
 
+    declare_launch_argument_sc = DeclareLaunchArgument(
+        'scan',
+        default_value='scan',
+        description='specify scan topic you want')
+
     stdout_log_use_stdout_envvar = SetEnvironmentVariable(
         'RCUTILS_LOGGING_USE_STDOUT', '1')
 
@@ -56,6 +63,7 @@ def generate_launch_description():
     ld.add_action(declare_launch_argument_sm)
     ld.add_action(declare_launch_argument_tm)
     ld.add_action(declare_launch_argument_tp)
+    ld.add_action(declare_launch_argument_sc)
     ld.add_action(cloisim_ros_cmd)
 
     return ld
