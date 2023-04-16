e8dc0929952c7b48b0c94f2025045babde92096d
(=YG=) Hyunseok Yang <hyunseok7.yang@lge.com>
2023-04-16 13:04:20 +0900

    Change default C++ version for compilation : 14 -> 17

--- cloisim_ros_base/cmake/cloisim_ros_package.cmake
+++ cloisim_ros_base/cmake/cloisim_ros_package.cmake
@@ -3,13 +3,14 @@ macro(cloisim_ros_package)
   if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
     message(STATUS "Setting build type to Release as none was specified.")
     set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build." FORCE)
+
     # Set the possible values of build type for cmake-gui
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
   endif()
 
-  # Default to C++14
+  # Default to C++17
   if(NOT CMAKE_CXX_STANDARD)
-    set(CMAKE_CXX_STANDARD 14)
+    set(CMAKE_CXX_STANDARD 17)
   endif()
 
   if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
