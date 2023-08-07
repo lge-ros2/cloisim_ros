3cd5873015e8001029dd897edd20ddda43ccc55b
(=YG=) Hyunseok Yang <hyunseok7.yang@lge.com>
2023-08-07 15:04:33 +0900

    Rename filename in closiim_ros_bridge_zmq debug_log.h -> log.h

--- cloisim_ros_bridge_zmq/include/cloisim_ros_bridge_zmq/log.h
+++ cloisim_ros_bridge_zmq/include/cloisim_ros_bridge_zmq/log.h
@@ -1,5 +1,5 @@
 /**
- *  @file   debug_log.h
+ *  @file   log.h
  *  @date   2020-04-21
  *  @author Hyunseok Yang
  *  @brief
@@ -12,20 +12,20 @@
  *
  *      SPDX-License-Identifier: MIT
  */
-#ifndef _DEBUG_LOG_H_
-#define _DEBUG_LOG_H_
+#ifndef _LOG_H_
+#define _LOG_H_
 
 #include "term_color.h"
 
 #pragma GCC system_header
 
-#define _DBG_SIM(COLOR_CODE, STR_FORMAT, ...) \
+#define __SIM_LOG(COLOR_CODE, STR_FORMAT, ...) \
   printf(COLOR_CODE "[%s] " STR_FORMAT RESET "\n", __FUNCTION__, ##__VA_ARGS__)
 //   printf(COLOR_CODE "[%s][%d] " STR_FORMAT RESET "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
-#define DBG_SIM_INFO(STR_FORMAT, ...) _DBG_SIM(CYAN, STR_FORMAT, ##__VA_ARGS__)
-#define DBG_SIM_WRN(STR_FORMAT, ...) _DBG_SIM(YELLOW, STR_FORMAT, ##__VA_ARGS__)
-#define DBG_SIM_MSG(STR_FORMAT, ...) _DBG_SIM(GREEN, STR_FORMAT, ##__VA_ARGS__)
-#define DBG_SIM_ERR(STR_FORMAT, ...) _DBG_SIM(BOLDRED, STR_FORMAT, ##__VA_ARGS__)
+#define DBG_SIM_INFO(STR_FORMAT, ...) __SIM_LOG(CYAN, STR_FORMAT, ##__VA_ARGS__)
+#define DBG_SIM_WRN(STR_FORMAT, ...) __SIM_LOG(YELLOW, STR_FORMAT, ##__VA_ARGS__)
+#define DBG_SIM_MSG(STR_FORMAT, ...) __SIM_LOG(GREEN, STR_FORMAT, ##__VA_ARGS__)
+#define DBG_SIM_ERR(STR_FORMAT, ...) __SIM_LOG(BOLDRED, STR_FORMAT, ##__VA_ARGS__)
 
 #endif
