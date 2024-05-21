3087710ea0c71695201733f3a9426e9113c3d74d
Hyunseok Yang (YG) <hyunseok7.yang@lge.com>
2024-05-21 11:01:47 +0900

    file renamed  - bringup.launch.py -> bringup_launch.py  - cloisim.launch.py -> cloisim_launch.py
    below two files will be deprecated.
    - bringup.launch.py, cloisim.launch.py

---
+++ cloisim_ros_bringup/launch/cloisim_launch.py
@@ -0,0 +1,42 @@
+#
+# LGE Advanced Robotics Laboratory
+# Copyright (c) 2020 LG Electronics Inc., LTD., Seoul, Korea
+# All Rights are Reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description() -> LaunchDescription:
+
+    sim_path = LaunchConfiguration('sim_path')
+    world = LaunchConfiguration('world')
+
+    execute_multi_robot_simulator = ExecuteProcess(
+        cmd=['./CLOiSim.x86_64', '-world', world],
+        cwd=[sim_path],
+        output='screen')
+
+    declare_launch_argument_sim_path = DeclareLaunchArgument(
+        'sim_path',
+        default_value='',
+        description='path where the CLOiSim simulator located')
+
+    declare_launch_argument_world = DeclareLaunchArgument(
+        'world',
+        default_value='',
+        description='It is World file name. Please check environments before run. [CLOISIM_WORLD_PATH, CLOISIM_MODEL_PATH]')
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Add the actions to launch all nodes
+    ld.add_action(declare_launch_argument_sim_path)
+    ld.add_action(declare_launch_argument_world)
+    ld.add_action(execute_multi_robot_simulator)
+
+    return ld
